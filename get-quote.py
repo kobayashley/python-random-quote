fa24e573a219710a0d77129243ebb257499a08df
kobayashley <a.mkobayashi@hotmail.com>
2020-08-15 23:40:09 -0700

    add quotes to quotes.txt

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,20 @@
 import random
 def primary():
 #  print("Keep it logically awesome.")
+  
+  file_object = open("quotes.txt", "a+")
+  file_object.write("\n")
+  file_object.write ("There is no substitute for hard work.")
+  file_object.close()
 
   f = open("quotes.txt")
   quotes = f.readlines() # stored in an array
   f.close()
 
 
-  last = 13;
-  rnd = random.randint(0,last);
-  print(quotes[rnd]);
+  last = 13
+  rnd = random.randint(0,last)
+  print(quotes[rnd])
 
 if __name__== "__main__": # don't change this
   primary() # can change what the main function is named
