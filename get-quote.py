2dc0058b5667ceaa8e69af0827f8d7f95ccb4c70
kobayashley <a.mkobayashi@hotmail.com>
2020-08-15 23:25:31 -0700

    python quotes are # not //

--- get-quote.py
+++ get-quote.py
@@ -2,10 +2,10 @@ def primary():
 #  print("Keep it logically awesome.")
 
   f = open("quotes.txt")
-  quotes = f.readlines() // stored in an array
+  quotes = f.readlines() # stored in an array
   f.close()
 
   print(quotes[0])
 
-if __name__== "__main__": // don't change this
-  primary() // can change what the main function is named
+if __name__== "__main__": # don't change this
+  primary() # can change what the main function is named
